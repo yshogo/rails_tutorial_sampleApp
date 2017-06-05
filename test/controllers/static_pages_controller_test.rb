278676b95614eaae67ff26645f39d7903d4d66d5
shogo.yamada <superman199323@gmail.com>
2017-06-05 09:26:09 +0900

    ヘルパーを使っていろいろやりましたyarimasita

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,18 +2,13 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
-  def setup
-    @base_title = "Ruby on Rails Tutorial Sample App"
-  end
-
-
   test "should get home" do
     #staticpageのhomeのURLにアクセスする
     get static_pages_home_url
     #成功します
     assert_response :success
     #タイトルが下記になっていること
-    assert_select "title", "Home | #{@base_title}"
+    assert_select "title", "Ruby on Rails Tutorial Sample App"
   end
 
   test "should get help" do
@@ -22,7 +17,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     #成功します
     assert_response :success
     #タイトルが下記になっていること
-    assert_select "title", "Help | #{@base_title}"
+    assert_select "title", "Help|Ruby on Rails Tutorial Sample App"
   end
 
   test "should get about" do
@@ -31,6 +26,6 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     #成功する
     assert_response :success
     #タイトルが下記になっていること
-    assert_select "title", "About | #{@base_title}"
+    assert_select "title", "About|Ruby on Rails Tutorial Sample App"
   end
 end
