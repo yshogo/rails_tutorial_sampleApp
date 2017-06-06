8ffa2c646971936f854d4fd8a5f07dbec71e4ce2
shogo.yamada <superman199323@gmail.com>
2017-06-06 16:39:22 +0900

    第7章終了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,31 @@
 class UsersController < ApplicationController
+
+  def show
+    @user = User.find(params[:id])
+  end
+
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      #保存成功パターン
+      if @user.save
+        flash[:success] = "Welcome to the Samples App!!"
+        redirect_to @user
+      end
+    else
+      #失敗パターンはリダイレクトされます
+      render 'new'
+    end
+  end
+
+
+  private
+  def user_params
+    params.require(:user).permit(:name,:email,:password,:password_confirm)
   end
 end
